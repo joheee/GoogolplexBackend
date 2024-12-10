07a125b24fed7d7c7ab4d3d539b013bf5f4f6065
joheee <johevinblesstowi07@gmail.com>
2024-12-10 23:23:55 +0700

    custom chaining endpoint post

--- src/api/user_class_member/dto/update-user_class_member.dto.ts
+++ src/api/user_class_member/dto/update-user_class_member.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateUserClassMemberDto } from './create-user_class_member.dto';
 
-export class UpdateUserClassMemberDto extends PartialType(CreateUserClassMemberDto) {}
+export class UpdateUserClassMemberDto extends PartialType(
+  CreateUserClassMemberDto,
+) {}
